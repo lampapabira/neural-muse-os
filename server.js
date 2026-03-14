8618eab02806124d89d8ef78fbc48ab1367af773
lampapabira <lampapa@hotmail.fr>
2026-03-14 17:28:53 +0100

    Add static file serving from 'public' directory

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(morgan("dev"));
+app.use(express.static("public"));
 
 // Limiteur de requêtes (Anti-spam / Anti-bot)
 const limiter = rateLimit({
